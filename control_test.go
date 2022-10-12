cd726a1a1883bab4ed69caa3c60da6991ff39f70
Abhinav Dangeti <abhinav@couchbase.com>
2022-10-12 15:17:18 -0600

    Add another control test demoing distribution of actives/replicas
    Change-Id: I86dc485418ba3c45e06cf8c73ccf997209fabf7d
    Reviewed-on: https://review.couchbase.org/c/blance/+/181228
    Tested-by: Abhi Dangeti <abhinav@couchbase.com>
    Reviewed-by: Abhi Dangeti <abhinav@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>

--- control_test.go
+++ control_test.go
@@ -304,3 +304,103 @@ func TestControlCase3(t *testing.T) {
 		t.Fatalf("Mismatch: %+v", rStr)
 	}
 }
+
+// Expect even distribution of actives and replicas
+func TestControlCase4(t *testing.T) {
+	NodeScoreBooster = func(w int, s float64) float64 {
+		// As defined in couchbase/cbgt
+		score := float64(-w)
+		if score < s {
+			score = s
+		}
+		return score
+	}
+	defer func() {
+		NodeScoreBooster = nil
+	}()
+
+	partitions := PartitionMap{
+		"X": &Partition{
+			Name: "X",
+			NodesByState: map[string][]string{
+				"primary": {"a"},
+				"replica": {"b"},
+			},
+		},
+		"Y": &Partition{
+			Name:         "Y",
+			NodesByState: map[string][]string{},
+		},
+	}
+
+	nodes := []string{"a", "b"}
+
+	model := PartitionModel{
+		"primary": &PartitionModelState{
+			Priority:    0,
+			Constraints: 1,
+		},
+		"replica": &PartitionModelState{
+			Priority:    1,
+			Constraints: 1,
+		},
+	}
+
+	r, warnings := PlanNextMapEx(
+		partitions,
+		nodes,
+		nil,
+		nil,
+		model,
+		PlanNextMapOptions{
+			ModelStateConstraints: nil,
+			PartitionWeights:      nil,
+			StateStickiness:       nil,
+			NodeWeights: map[string]int{
+				"a": -1,
+				"b": -1,
+			},
+			NodeHierarchy: map[string]string{
+				"a": "Group 1",
+				"b": "Group 2",
+			},
+			HierarchyRules: HierarchyRules{
+				"replica": []*HierarchyRule{
+					{
+						IncludeLevel: 2,
+						ExcludeLevel: 1,
+					},
+				},
+			},
+		})
+
+	if len(warnings) > 0 {
+		t.Errorf("WARNINGS: %v", warnings)
+	}
+
+	expect := PartitionMap{
+		"X": &Partition{
+			Name: "X",
+			NodesByState: map[string][]string{
+				"primary": {"a"},
+				"replica": {"b"},
+			},
+		},
+		"Y": &Partition{
+			Name: "Y",
+			NodesByState: map[string][]string{
+				"primary": {"b"},
+				"replica": {"a"},
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(r, expect) {
+		var rStr string
+		for k, v := range r {
+			rStr += fmt.Sprintf("%+v: %+v", k, v)
+		}
+
+		t.Fatalf("Mismatch: %+v", rStr)
+	}
+}
